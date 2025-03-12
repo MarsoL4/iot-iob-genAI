ca0bfef472366bab3756e4441e60f45e5cc3c153
MarsoL4 <enzogmarsola@gmail.com>
2025-03-12 11:07:53 -0300

    feat: métodos de resolução dos exercícios 4 e 5

--- 12-3/exercicios12-3.ino
+++ 12-3/exercicios12-3.ino
@@ -17,14 +17,56 @@ void setup(){
   digitalWrite(ledB, LOW);
   digitalWrite(ledW, LOW);
   digitalWrite(ledR, LOW);
+  
+  delay(3000);
 
 
 }
 
 void loop() {
-  
+  //ex4();
+  //ex5();
 }
 
 void ex4(){
+  digitalWrite(ledR, HIGH);
+  delay(1000);
+  digitalWrite(ledW, HIGH);
+  delay(1000);
+  digitalWrite(ledB, HIGH);
 
+  delay(3000);
+
+  digitalWrite(ledB, LOW);
+  digitalWrite(ledW, LOW);
+  digitalWrite(ledR, LOW);
+
+  delay(2000)
 }
+
+void ex5(){
+  digitalWrite(ledB, HIGH);
+  delay(1000);
+  digitalWrite(ledB, LOW);
+  digitalWrite(ledW, HIGH);
+  delay(1000);
+  digitalWrite(ledW, LOW);
+  digitalWrite(ledR, HIGH);
+  delay(1000)
+  digitalWrite(ledR, LOW);
+
+  delay(3000)
+}
+
+void ex6(){
+  
+}
+
+
+
+
+
+
+
+
+
